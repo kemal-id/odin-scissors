114abbae3d2349f91f056c69ec880662e993585f
kemal-id <54977402+kemal-id@users.noreply.github.com>
2025-04-30 17:28:57 +0700

    Write logic for human choice

--- scissors.js
+++ scissors.js
@@ -3,8 +3,7 @@ const PAPER = "PAPER";
 const SCISSOR = "SCISSOR";
 
 function getComputerChoice() {
-  let mathChance = Math.random();
-  // console.log(mathChance);
+  let mathChance = Math.random().toFixed(3);
 
   if (mathChance <= 0.333) {
     return ROCK;
@@ -12,5 +11,23 @@ function getComputerChoice() {
     return PAPER;
   } else {
     return SCISSOR;
+  }
+}
+
+function getHumanChoice() {
+  let message =
+    "write 1 for Rock, 2 for Scissors, 3 for Paper, otherwise it's random";
+  let humanChoice = window.prompt(message);
+  let intHumanChoice = parseInt(humanChoice);
+
+  switch (intHumanChoice) {
+    case 1:
+      return ROCK;
+    case 2:
+      return SCISSOR;
+    case 3:
+      return PAPER;
+    default:
+      return getComputerChoice();
   }
 }
