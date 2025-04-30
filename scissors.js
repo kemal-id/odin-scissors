512f47a1e36308af0c012dbdb682680b65d04f84
kemal-id <54977402+kemal-id@users.noreply.github.com>
2025-04-30 17:04:17 +0700

    Write logic for computer choice

--- scissors.js
+++ scissors.js
@@ -1,1 +1,16 @@
-console.log("this works!");
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSOR = "SCISSOR";
+
+function getComputerChoice() {
+  let mathChance = Math.random();
+  // console.log(mathChance);
+
+  if (mathChance <= 0.333) {
+    return ROCK;
+  } else if (mathChance > 0.333 && mathChance <= 0.666) {
+    return PAPER;
+  } else {
+    return SCISSOR;
+  }
+}
